4756352ce872193d32b8d89229df754d0490a566
Kinsama <shine910816@163.com>
2018-12-31 23:05:20 +0800

    modify return type

--- api/menu/UsrApi/act/IohUsrApi_SendVerifyCodeAction.php
+++ api/menu/UsrApi/act/IohUsrApi_SendVerifyCodeAction.php
@@ -16,26 +16,31 @@ class IohUsrApi_SendVerifyCodeAction
      */
     public function doMainExecute(Controller $controller, User $user, Request $request)
     {
-        header("Content-type:text/plain; charset=utf-8");
-        $result = array(
-            "error" => 0,
-            "err_msg" => ""
-        );
+        //header("Content-type:text/plain; charset=utf-8");
+        //$result = array(
+        //    "error" => 0,
+        //    "err_msg" => ""
+        //);
+        //$exec_result = $this->_doDefaultExecute($controller, $user, $request);
+        //if ($controller->isError($exec_result)) {
+        //    $exec_result->setPos(__FILE__, __LINE__);
+        //    $error_message = "";
+        //    if ($exec_result->err_code == ERROR_CODE_DATABASE_RESULT) {
+        //        $error_message = "数据库发生错误";
+        //    } else {
+        //        $error_message = $exec_result->getMessage();
+        //    }
+        //    $result["error"] = 1;
+        //    $result["err_msg"] = $error_message;
+        //    $exec_result->writeLog();
+        //}
+        //echo json_encode($result);
+        //exit;
         $exec_result = $this->_doDefaultExecute($controller, $user, $request);
         if ($controller->isError($exec_result)) {
             $exec_result->setPos(__FILE__, __LINE__);
-            $error_message = "";
-            if ($exec_result->err_code == ERROR_CODE_DATABASE_RESULT) {
-                $error_message = "数据库发生错误";
-            } else {
-                $error_message = $exec_result->getMessage();
-            }
-            $result["error"] = 1;
-            $result["err_msg"] = $error_message;
-            $exec_result->writeLog();
         }
-        echo json_encode($result);
-        exit;
+        return $exec_result;
     }
 
     /**

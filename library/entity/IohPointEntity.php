71c4444e1a26dca9a96bc62b6b2e6004436ede17
shine910816 <shine910816@163.com>
2019-01-09 14:19:44 +0800

    add point type for birthday

--- library/entity/IohPointEntity.php
+++ library/entity/IohPointEntity.php
@@ -11,6 +11,7 @@ class IohPointEntity
     const POINT_TYPE_SIGNIN = "1";
     const POINT_TYPE_SITE_SERVICE = "2";
     const POINT_TYPE_COMPENSATE = "3";
+    const POINT_TYPE_BIRTHDAY = "4";
 
     public static function getEntityName()
     {
@@ -23,7 +24,8 @@ class IohPointEntity
             self::POINT_TYPE_REGISTER => "注册奖励活动",
             self::POINT_TYPE_SIGNIN => "签到",
             self::POINT_TYPE_SITE_SERVICE => "站内服务",
-            self::POINT_TYPE_COMPENSATE => "系统补偿"
+            self::POINT_TYPE_COMPENSATE => "系统补偿",
+            self::POINT_TYPE_BIRTHDAY => "生日赠礼"
         );
     }
 }

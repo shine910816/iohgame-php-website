da1d28e70b6c05364856a627ddf7cfcb75f027a8
Kinsama <shine910816@163.com>
2019-01-23 00:47:06 +0800

    add

---
+++ library/entity/IohSgsgzCardEntity.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * 数据库应用类-g_sgsgz_card_*
+ * @author Kinsama
+ * @version 2019-01-22
+ */
+class IohSgsgzCardEntity
+{
+    const CARD_TYPE_BASIC = "1";
+    const CARD_TYPE_EQUIT = "2";
+    const CARD_TYPE_MAGIC = "3";
+
+    const EQUIT_TYPE_NONE = "0";
+    const EQUIT_TYPE_WEAPON = "1";
+    const EQUIT_TYPE_HELMET = "2";
+    const EQUIT_TYPE_HORSE = "3";
+    const EQUIT_TYPE_TREASURE = "4";
+    
+    const MAGIC_DELEY_YES = "1";
+    const MAGIC_DELEY_NO = "0";
+
+    const RESET_YES = "1";
+    const RESET_NO = "0";
+
+    const HORIZ_YES = "1";
+    const HORIZ_NO = "0";
+}
+?>

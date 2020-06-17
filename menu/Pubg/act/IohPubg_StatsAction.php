6f10955c62dd82135eb4ceeac981efb1d1708047
shine910816 <shine910816@163.com>
2020-06-17 17:28:25 +0800

    a

--- menu/Pubg/act/IohPubg_StatsAction.php
+++ menu/Pubg/act/IohPubg_StatsAction.php
@@ -44,8 +44,13 @@ class IohPubg_StatsAction extends ActionBase
             $controller->redirect("./?menu=pubg&act=bind_account");
             return VIEW_DONE;
         }
+        $fpp_flg = false;
+        if ($request->hasParameter("fpp_mode")) {
+            $fpp_flg = true;
+        }
         $account_id = $account_list[$custom_id]["account_id"];
         $request->setAttribute("account_id", $account_id);
+        $request->setAttribute("fpp_flg", $fpp_flg);
         return VIEW_DONE;
     }
 
@@ -63,24 +68,74 @@ class IohPubg_StatsAction extends ActionBase
             $stats_info->setPos(__FILE__, __LINE__);
             return $stats_info;
         }
-//Utility::testVariable($stats_info);
+Utility::testVariable($stats_info);
         return VIEW_DONE;
     }
 
     private function _getStats(Controller $controller, User $user, Request $request)
     {
         $account_id = $request->getAttribute("account_id");
+        $fpp_flg = $request->getAttribute("fpp_flg");
+        $result = array(
+            "season" => array(),
+            "ranked" => array()
+        );
         $ranked_info = $this->_pubg->getPlayerRankedStats($account_id);
         if ($controller->isError($ranked_info)) {
             $ranked_info->setPos(__FILE__, __LINE__);
-            return $ranked_info;
+            return $result;
         }
         $season_info = $this->_pubg->getPlayerSeasonStats($account_id);
         if ($controller->isError($season_info)) {
             $season_info->setPos(__FILE__, __LINE__);
-            return $season_info;
+            return $result;
         }
-//Utility::testVariable($season_info);
+        $game_type_list = explode(",", "solo,duo,squad");
+        if (isset($season_info["data"]["attributes"]["gameModeStats"])) {
+            $season_data = $season_info["data"]["attributes"]["gameModeStats"];
+            $result["season"] = array(
+                "roundsPlayed" => "0",        //场数
+                "top10s" => "0",              //前十场数
+                "wins" => "0",                //吃鸡
+                "damageDealt" => "0",         //伤害
+                "timeSurvived" => "0",        //生存时间
+                "dBNOs" => "0",               //击倒
+                "kills" => "0",               //击杀
+                "headshotKills" => "0",       //爆头击败
+                "roadKills" => "0",           //载具击败
+                "assists" => "0",             //助攻
+                "revives" => "0",             //复活
+                "heals" => "0",               //治疗
+                "boosts" => "0",              //加速
+                "losses" => "0",              //死亡
+                "teamKills" => "0",           //队友击败
+                "suicides" => "0",            //自杀
+                "walkDistance" => "0",        //步行距离
+                "swimDistance" => "0",        //游泳距离
+                "rideDistance" => "0",        //驾驶距离
+                "vehicleDestroys" => "0",     //载具破坏
+                "weaponsAcquired" => "0",     //武器拾取
+                "roundMostKills" => "0",      //单场最高击败
+                "maxKillStreaks" => "0",      //最高连杀
+                "longestKill" => "0",         //单场最高击败距离
+                "longestTimeSurvived" => "0"  //单场最高生存时间
+            );
+            foreach ($game_type_list as $game_type) {
+                $data_key = $game_type;
+                if ($fpp_flg) {
+                    $data_key .= "-fpp";
+                }
+                if (empty($result["season"])) {
+                    $result["season"] = $season_data[$data_key];
+                } else {
+                    foreach ($season_data[$data_key] as $stats_key => $stats_value) {
+                        $result["season"][$stats_key] += $stats_value;
+                    }
+                }
+            }
+        }
+Utility::testVariable($season_info);
+        return $result;
     }
 }
 ?>
--- menu/Pubg/tpl/IohPubg_StatsMobileView.tpl
+++ menu/Pubg/tpl/IohPubg_StatsMobileView.tpl
@@ -1,42 +1,19 @@
 {^include file=$mblheader_file^}
-<div data-role="tabs" id="tabs">
-  <div data-role="navbar">
-    <ul>
-      <li><a href="#season" data-ajax="false">休闲模式</a></li>
-      <li><a href="#ranked" data-ajax="false">竞技模式</a></li>
-    </ul>
-  </div>
-  <div id="season" class="ui-body-d ui-content">
-    <div data-role="tabs" id="season_tabs">
-      <div data-role="navbar">
-        <ul>
-          <li><a href="#season-tpp" data-ajax="false">TPP</a></li>
-          <li><a href="#season-fpp" data-ajax="false">FPP</a></li>
-        </ul>
-      </div>
-      <div id="season-tpp" class="ui-body-d ui-content">
-        <h3>休闲-TPP</h3>
-      </div>
-      <div id="season-fpp" class="ui-body-d ui-content">
-        <h3>休闲-FPP</h3>
-      </div>
+<a href="./?menu=pubg&act=stats{^if !$fpp_flg^}&fpp_mode=1{^/if^}" class="ui-btn ui-corner-all ui-btn-{^if $fpp_flg^}b{^else^}a{^/if^}" data-ajax="false">FPP</a>
+<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-collapsed="false">
+  <h4>技术统计</h4>
+  <div data-role="tabs" id="tabs">
+    <div data-role="navbar">
+      <ul>
+        <li><a href="#season" data-ajax="false">休闲模式</a></li>
+        <li><a href="#ranked" data-ajax="false">竞技模式</a></li>
+      </ul>
     </div>
-  </div>
-  <div id="ranked" class="ui-body-d ui-content">
-    <div data-role="tabs" id="season_tabs">
-      <div data-role="navbar">
-        <ul>
-          <li><a href="#ranked-tpp" data-ajax="false">TPP</a></li>
-          <li><a href="#ranked-fpp" data-ajax="false">FPP</a></li>
-        </ul>
-      </div>
-      <div id="ranked-tpp" class="ui-body-d ui-content">
-        <h3>竞技-TPP</h3>
-      </div>
-      <div id="ranked-fpp" class="ui-body-d ui-content">
-        <h3>竞技-FPP</h3>
-      </div>
+    <div id="season" class="ui-body-d ui-content">
+    </div>
+    <div id="ranked" class="ui-body-d ui-content">
     </div>
   </div>
 </div>
+<a href="./?menu=pubg&act=bind_account" class="ui-btn ui-corner-all ui-btn-a" data-ajax="false">修改绑定角色</a>
 {^include file=$mblfooter_file^}
